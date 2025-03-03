600b166ae4940e3df12d21c985aa0374b922e8e1
zh33nbekov <rai.zheenbekov@gmail.com>
2025-03-04 00:10:52 +0600

    fixed bug in error.middleware.js

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,6 +1,6 @@
 const ApiError = require('../exceptions/api.error.exception')
 
-const ErrorMiddleware = (err, req, res) => {
+const ErrorMiddleware = (err, req, res, next) => {
 	if (err instanceof ApiError) {
 		return res.status(err.status).json({ message: err.message, errors: err.errors })
 	}
